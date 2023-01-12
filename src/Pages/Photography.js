52a2b2536f44e48093b5de321111402eb6afc955
adcva <onackirema@gmail.com>
2023-01-12 18:49:02 +0200

    Component

--- src/Pages/Photography.js
+++ src/Pages/Photography.js
@@ -3,8 +3,123 @@ import Nav from "../Components/Nav";
 
 function Photography() {
   return (
-    <div>
-      <Nav />
+    <div className='photography-wrapper'>
+      <div className='pages-nav-container'>
+        <Nav />
+      </div>
+      <div className='photography-content-container'>
+
+        {/* ====================== 1 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/1.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 2 ====================== */}
+        <div>
+          <img src="./images/2.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 3 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/3.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 4 ====================== */}
+        <div>
+          <img src="./images/4.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 5 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/5.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 6 ====================== */}
+        <div>
+          <img src="./images/6.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 7 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/7.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 8 ====================== */}
+        <div>
+          <img src="./images/8.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 9 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/9.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 10 ====================== */}
+        <div>
+          <img src="./images/10.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 11 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/11.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 12 ====================== */}
+        <div>
+          <img src="./images/12.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 13 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/13.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 14 ====================== */}
+        <div>
+          <img src="./images/14.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 15 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/15.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 16 ====================== */}
+        <div>
+          <img src="./images/16.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 17 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/17.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 18 ====================== */}
+        <div>
+          <img src="./images/18.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 19 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/19.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 20 ====================== */}
+        <div>
+          <img src="./images/20.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 21 ====================== */}
+        <div className='vertical-stretch'>
+          <img src="./images/21.jpg" alt="Slide"/>
+        </div>
+
+        {/* ====================== 22 ====================== */}
+        <div>
+          <img src="./images/22.jpg" alt="Slide"/>
+        </div>
+
+      </div>
     </div>
   )
 }
