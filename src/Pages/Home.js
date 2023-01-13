6731d628c270d2653dae9c1c3a8760bd9fc904f8
adcva <onackirema@gmail.com>
2023-01-13 22:39:58 +0200

    Test Navbar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Nav from "../Components/Nav";
+import Navbar from '../Components/Navbar';
 
 function Home() {
   return (
     <div>
-      <Nav />
+      <Navbar />
     </div>
   )
 }
