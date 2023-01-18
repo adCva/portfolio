75730504621a408617233f05fed927596202312f
adcva <onackirema@gmail.com>
2023-01-18 03:16:39 +0200

    Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import './App.css';
+import './Reset.css';
 import "./Scss/style.css";
-import Slide from "./Components/Slide";
+import { BrowserRouter, Routes, Route}  from 'react-router-dom';
 import Home from "./Pages/Home";
 import Projects from "./Pages/Projects";
 import Photography from "./Pages/Photography";
 import Contact from "./Pages/Contact";
-import { BrowserRouter, Routes, Route}  from 'react-router-dom';
-
 
 function App() {
   return (
     <BrowserRouter basename={window.location.pathname || ''} className="App">
       <Routes>
-        <Route exact path="/" element={<Slide />}/>
-        <Route path="/projects" element={<Projects />}/>
-        <Route path="/photography" element={<Photography />}/>
-        <Route path="/contact" element={<Contact />}/>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/projects" element={<Projects />}/>
+        <Route exact path="/photography" element={<Photography />}/>
+        <Route exact path="/contact" element={<Contact />}/>
       </Routes>
     </BrowserRouter>
   );
